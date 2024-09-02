13a2a207329fc8cf29c22d294349dd2236019eb8
trsv.egor_FX505DT <eg0r-taras0v@yandex.ru>
2024-09-02 21:37:23 +0300

    -Промежуточный- Сделано задание с темами и с Intent

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,13 +18,49 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         setTitle(R.string.settings_name)
-        toolbar.setNavigationIcon(R.drawable.arrowback)
+        toolbar.setTitleTextAppearance(this, R.style.ToolbarStyle)
+        toolbar.setNavigationIcon(R.drawable.navigate_arrowback)
         toolbar.setNavigationOnClickListener { finish() }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Share app button
+        val buttonShare = findViewById<TextView>(R.id.share)
+
+        buttonShare.setOnClickListener{
+            val intentShare = Intent(Intent.ACTION_SEND)
+            intentShare.putExtra(Intent.EXTRA_TEXT, R.string.course_url)
+            intentShare.setType("text/plain");
+            startActivity(intentShare)
+        }
+
+        //Support button
+        val buttonSupport = findViewById<TextView>(R.id.support)
+
+        buttonSupport.setOnClickListener{
+            //тут херня
+            val emailSubject = R.string.email_subject
+            val emailText = R.string.email_text
+            val intentEmail = Intent(Intent.ACTION_SENDTO)
+            intentEmail.data = Uri.parse("mailto:")
+            intentEmail.putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.my_email))
+            intentEmail.putExtra(Intent.EXTRA_SUBJECT, emailSubject)
+            intentEmail.putExtra(Intent.EXTRA_TEXT, emailText)
+            startActivity(intentEmail)
+        }
+
+        //User Agreement button
+        val buttonUserAgreement = findViewById<TextView>(R.id.user_agreement)
+
+        buttonUserAgreement.setOnClickListener {
+            val intentBrowser = Intent(Intent.ACTION_VIEW)
+            intentBrowser.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(intentBrowser)
         }
     }
 }

4b23f9d6133304c6205b539610c95a33f2453cfd
trsv.egor_FX505DT <eg0r-taras0v@yandex.ru>
2024-09-04 02:26:28 +0300

    -Промежуточный- 2

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +18,36 @@ class SearchActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
+        val linearLayout = findViewById<LinearLayout>(R.id.search)
+        val inputEditText = findViewById<EditText>(R.id.search_field)
+        val clearButton = findViewById<ImageView>(R.id.search_clear)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+
+                } else {
+
+                }
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+
+
         val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         toolbar.setNavigationIcon(R.drawable.toolbar_arrowback)
@@ -20,10 +56,19 @@ class SearchActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener { finish() }
 
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }

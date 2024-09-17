34f85341366f5a9bdc16013d141fe4d350cf149b
trsv.egor_FX505DT <eg0r-taras0v@yandex.ru>
2024-09-17 12:59:23 +0300

    Спринт 10: Исправления 1. Поправлены gitignore 2. Выполнено форматирование кода, удалены лишние import 3. Из SearchActivity.kt вынесены в отдельные файлы классы TrackAdapter, TrackMockObject, TrackViewHolder. 4.Испралено имя файла settings_track_arrow_forward.xml

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,28 +1,20 @@
 package com.practicum.playlistmaker
 
-import android.R.dimen
+
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.TypedValue
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.practicum.playlistmaker.databinding.ActivitySearchBinding
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
 
 class SearchActivity : AppCompatActivity() {
 
@@ -31,38 +23,7 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        val trackArray = listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-            )
-        )
+        val trackArray = TrackMockObject.getTracks()
 
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
@@ -140,52 +101,4 @@ class SearchActivity : AppCompatActivity() {
         const val SEARCH_FIELD_TEXT = "SEARCH_FIELD_TEXT"
         const val SEARCH_FIELD_DEF = ""
     }
-}
-
-class TrackViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
-
-    private val trackLogo: ImageView = itemview.findViewById(R.id.view_track_logo)
-    private val trackName: TextView = itemview.findViewById(R.id.view_track_name)
-    private val trackArtist: TextView = itemview.findViewById(R.id.view_track_artist)
-    private val trackLength: TextView = itemview.findViewById(R.id.view_track_length)
-
-    fun bind(model: Track) {
-
-        Glide.with(trackLogo.context)
-            .load(model.artworkUrl100)
-            .placeholder(R.drawable.track_placeholder)
-            .transform(RoundedCorners(dpToPx(2f, trackLogo.context)))
-            .into(trackLogo)
-
-        trackName.text = model.trackName
-        trackArtist.text = model.artistName
-        trackLength.text = model.trackTime
-
-    }
-}
-
-class TrackAdapter(
-    private val trackList: List<Track>
-) : RecyclerView.Adapter<TrackViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_track, parent, false)
-        return TrackViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        holder.bind(trackList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return trackList.size
-    }
-}
-
-fun dpToPx(dp: Float, context: Context): Int {
-    return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP,
-        dp,
-        context.resources.displayMetrics
-    ).toInt()
 }
